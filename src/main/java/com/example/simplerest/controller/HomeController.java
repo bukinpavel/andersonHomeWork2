5fee86cd02511079aef3dfdbffa09f64ab3edf15
Wiggmund <mirracleourworld@gmail.com>
2023-07-18 17:12:13 +0300

    Trigger 7

--- src/main/java/com/example/simplerest/controller/HomeController.java
+++ src/main/java/com/example/simplerest/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("home")
     public ResponseEntity<String> homePage() {
-        return ResponseEntity.ok("Just trying GithubHook for Jenkins #6");
+        return ResponseEntity.ok("Just trying GithubHook for Jenkins #7");
     }
 }
